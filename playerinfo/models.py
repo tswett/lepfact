0bc15dbe156978d58565b0772d75df543c96c0ec
Tanner Swett <tannerswett@gmail.com>
2014-03-15 01:39:00 +0000

    Change ForeignKey to OneToOneKey in UserProfile
    The field in UserProfile pointing to the corresponding User was a ForeignKey with unique=True; replace it with a OneToOneKey.

--- playerinfo/models.py
+++ playerinfo/models.py
@@ -3,7 +3,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class UserProfile(models.Model):
-    user = models.ForeignKey(User, unique=True)
+    user = models.OneToOneField(User)
     unix_uid = models.BigIntegerField(null=True, blank=True, unique=True)
     unix_username = models.CharField(max_length=32, null=True, blank=True, unique=True)
     sql_username = models.CharField(max_length=32, null=True, blank=True, unique=True)
