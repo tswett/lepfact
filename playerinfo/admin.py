4c2a6a1559db5737b1713fc6b179d5f3c44d09fa
Tanner Swett <tannerswett@gmail.com>
2014-03-23 00:31:01 +0000

    Replace factory classes with FactoryType class

--- playerinfo/admin.py
+++ playerinfo/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from playerinfo.models import UserProfile, Currency, Account, Transaction
+from playerinfo.models import *
 
 admin.site.register(UserProfile)
 admin.site.register(Currency)
 admin.site.register(Account)
 admin.site.register(Transaction)
+admin.site.register(FactoryType)
