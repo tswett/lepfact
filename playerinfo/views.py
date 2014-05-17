88736cea2fe235bd1a1fb95ad68d8edaa61a7f84
Tanner Swett <tannerswett@gmail.com>
2014-05-16 21:39:59 -0400

    Add factory type listing to player info page

--- playerinfo/views.py
+++ playerinfo/views.py
@@ -1,11 +1,52 @@
 from django.http import HttpResponse
 import django.shortcuts
 
-from playerinfo.models import Account, UserProfile
+from playerinfo.models import (Account, UserProfile, FactoryType,
+    BuildCostData, StartupCostData, IdleUpkeepData, ActiveUpkeepData, YieldData)
+
+COST_CLASS = {
+    'Build cost': BuildCostData,
+    'Startup cost': StartupCostData,
+    'Idle upkeep': IdleUpkeepData,
+    'Active upkeep': ActiveUpkeepData,
+    'Yield': YieldData,
+}
 
 def list_profiles(request):
     profiles = UserProfile.objects.all()
+
     accounts = Account.objects.all()
 
-    context = {'profiles': profiles, 'accounts': accounts}
+    factory_types = []
+
+    for factory_type in FactoryType.objects.all():
+        costtypes = []
+
+        for costtype_name in COST_CLASS:
+            cost_class = COST_CLASS[costtype_name]
+
+            costs = cost_class.objects.filter(factory_type=factory_type)
+
+            if not costs:
+                continue
+
+            costtype_info = {'name': costtype_name, 'costs': []}
+
+            for cost in costs:
+                cost_info = {'name': cost.currency.name, 'amount': cost.amount}
+                costtype_info['costs'].append(cost_info)
+
+            costtypes.append(costtype_info)
+
+        factory_types.append({
+            'name': factory_type.name,
+            'costtypes': costtypes,
+        })
+
+    context = {
+        'profiles': profiles,
+        'accounts': accounts,
+        'factory_types': factory_types,
+    }
+
     return django.shortcuts.render(request, 'playerinfo/index.html', context)
